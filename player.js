d71b3d9563ee00502cf6a4e513c8916a74e123ce
sumonali9010 <sumonali9010@gmail.com>
2026-01-16 07:09:35 +0600

    Update player.js

--- player.js
+++ player.js
@@ -1,33 +1,5 @@
 fetch('channels.m3u')
-  .then(res => res.text())
-  .then(data => {
-    const lines = data.split('\n');
-    const select = document.getElementById('channelList');
-    let name = '';
-
-    lines.forEach(line => {
-      if (line.startsWith('#EXTINF')) {
-        name = line.split(',')[1];
-      }
-      if (line.startsWith('http')) {
-        const opt = document.createElement('option');
-        opt.value = line.trim();
-        opt.text = name;
-        select.appendChild(opt);
-      }
-    });
-
-    play(select.value);
-    select.onchange = () => play(select.value);
+  .then(r => r.text())
+  .then(playList => {
+    // parse M3U8 & inject DOM
   });
-
-function play(url) {
-  const video = document.getElementById('video');
-  if (Hls.isSupported()) {
-    const hls = new Hls();
-    hls.loadSource(url);
-    hls.attachMedia(video);
-  } else {
-    video.src = url;
-  }
-}
